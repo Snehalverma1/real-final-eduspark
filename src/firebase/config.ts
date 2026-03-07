88eb2ca8b1dac628b5ec66e1f6150161568669b6
Snehal Verma <vermasnehal045@gmail.com>
2026-03-07 17:49:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1862763050-d454f",
+  "appId": "1:898327182539:web:e7451527aa304714426f4e",
+  "apiKey": "AIzaSyAGF0HljVbGROVaaN5XUjon_bXH7eXY_zM",
+  "authDomain": "studio-1862763050-d454f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "898327182539"
+};
