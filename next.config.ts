5bca4c86af3e2d550fcb1cc3e4a563af83724a62
Snehal Verma <vermasnehal045@gmail.com>
2026-05-06 15:38:00 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -26,12 +26,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.devServer = { ...config.devServer, hot: { websocketProtocol: 'ws' } };
-    }
-    return config;
   },
 };
 
